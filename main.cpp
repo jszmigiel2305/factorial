bd44005d72e7325d832a0a454b670c5cab6e5958
Jakub Szmigiel <jszmigiel2304@outlook.com>
2022-12-10 16:01:26 +0100

    dasd

--- main.cpp
+++ main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include <array>
+#include <algorithm>
+#include <stdlib.h>
+#include <time.h>
+#include <compare>
 
-using namespace std;
+
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    srand(time(NULL));
+    int comp{0};
+    std::vector<int> v;
+
+    for(int i{0}; i<4000000; i++)
+        v.push_back(rand());
+
+    std::qsort(&v, v.size(), sizeof(int),
+        [&](const void* x, const void* y) {
+        const int arg1 = *static_cast<const int*>(x);
+        const int arg2 = *static_cast<const int*>(y);
+        const auto cmp = arg1 <=> arg2;
+        if (cmp < 0) return -1;
+        if (cmp > 0) return 1;
+        return 0;
+    });
+
+    std::for_each(v.begin(), v.end(), [](const int &i){std::cout << i << " asdf " << std::endl;});
+    std::cout << comp << std::endl;
+
+  return 0;
 }
